7e63d9204e364db54ab0961fc06102e3412840e0
Adam Abdulhafid <adam.m.abdulhafid@gmail.com>
2015-09-26 11:46:47 -0400

    Updated files for final app

--- server.R
+++ server.R
@@ -1,13 +1,40 @@
-data(mtcars)
-
-mpgpredict <- lm(mpg ~ cyl + hp + gear, data = mtcars)
+predictmpg <- function(cyl, hp, gear) {
+        data(mtcars)
+        mtcars$hp <- mtcars$hp/50
+        mpgpredict <- lm(mpg ~ cyl + hp + gear, data = mtcars)
+        inputs <- data.frame("cyl" = cyl,
+                             "hp" = hp,
+                             "gear" = gear)
+        round(predict(mpgpredict, newdata = inputs), 0)
+}
 
 shinyServer(
         function(input, output) {
-                data.frame("cyl" = {input$cyl},
-                           "hp" = {input$hp},
-                           "gear" = {input$gear})
-                mpgpred <- round(predict(mpgpredict, newdata = inputs), 0)
-                output$prediction <- renderPrint(mpgpred)
+                # Reactive transformation of horsepower
+                hpreact <- reactive({input$hp/50})
+                # Ouput for number of cylinders
+                output$inputCyl <- renderPrint({
+                        paste("Number of Cylinders:",
+                              as.numeric(input$cyl))
+                        })
+                # Output for transformed horsepower
+                output$inputHp <- renderPrint({
+                        paste("Transformed Horsepower:",
+                              hpreact())
+                })
+                # Output for number of gears
+                output$inputGear <- renderPrint({
+                        paste("Number of Gears:",
+                              as.numeric(input$gear))
+                })
+
+                npredict <- eventReactive(input$goButton, {
+                        predictmpg(as.numeric(input$cyl),
+                                   hpreact(),
+                                   as.numeric(input$gear))                        
+                })
+                output$prediction <- renderPrint({
+                        npredict()
+                })
         }
 )
--- ui.R
+++ ui.R
@@ -1,15 +1,47 @@
 shinyUI(pageWithSidebar(
+        # Title of the application
         headerPanel("Predicting MPG"),
+        
         sidebarPanel(
-                h1("Car Attributes"),
+                h3("Car Attributes"),
+                
+                # Explanation of the app
+                p("This application uses the below attributes of a 
+                  car to predict what the mpg will be for it. The 
+                  model is a simple linear model using the mtcars 
+                  dataset in R. The horsepower attribute is divided 
+                  by 50 to align with the model. The values for the 
+                  attributes will appear in the main panel for you 
+                  to review prior to running the prediction."),
+                # Input for cylinders
+                radioButtons("cyl", "Number of Cylinders:", 
+                             c(4, 6, 8)),
+                p("Click on the radio button to choose the
+                   number of cylinders in your car."),
+                # Input for horsepower
+                sliderInput("hp", "How much Horsepower:",
+                             150, min = 100, max = 300, step = 5),
+                p("Slide the slider to the amount of horsepower 
+                  in your car."),
+                # Input for gears
+                selectInput("gear", "How many Gears:", 4:8),
+                p("Select the number of gears in your car between 
+                  4 and 8."),
+                # Initiate prediction button
+                actionButton("goButton", "Submit"),
+                p("Once you are satisfied with all of the attribute 
+                  values, please click the submit button to receive 
+                  your prediction.")
         ),
-        radioButtons("cyl", "Number of Cylinders:", 
-                     "4" = "4",
-                     "6" = "6",
-                     "8" = "8", selected = NULL, inline = FALSE),
-        numericInput("hp", "Horsepower:", 150, min = 100, max = 300, step = 1),
         
         mainPanel(
-                h1("Prediction"),
+                h3("Prediction"),
+                h4("Your car is detailed by"),
+                verbatimTextOutput("inputCyl"),
+                verbatimTextOutput("inputHp"),
+                verbatimTextOutput("inputGear"),
+                h4("Your expected MPG is"),
+                verbatimTextOutput("prediction")
+                
         )
 ))
